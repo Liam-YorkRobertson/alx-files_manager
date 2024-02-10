c05320d6cdab24c927866cb3c5f4c44e4b08bc26
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-02-10 20:17:17 +0200

    connected to mongodb and read doc info

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+// connects to mongodb and get info of documents
+
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.db = null;
+    this.connect();
+  }
+
+  async connect() {
+    await this.client.connect();
+    console.log('Connected to MongoDB');
+    this.db = this.client.db();
+  }
+
+  isAlive() {
+    return this.db !== null;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
