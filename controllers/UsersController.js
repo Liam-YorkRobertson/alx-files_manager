7b5e2a18ce4bd34596cd4491aa489a06beae198c
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-02-12 02:03:18 +0200

    add user to db

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,33 +1,33 @@
-// controllers/UsersController.js
+// user controller
 
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 const UsersController = {
-  postNew: (req, res) => {
+  postNew: async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Missing email or password' });
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
     }
 
-    dbClient.users.findOne({ email }, (err, existingUser) => {
-      if (err) {
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-      if (existingUser) {
-        return res.status(400).json({ error: 'User already exists' });
-      }
+    const existingUser = await dbClient.users.findOne({ email });
 
-      const hashedPassword = sha1(password);
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
 
-      dbClient.users.insertOne({ email, password: hashedPassword }, (err, newUser) => {
-        if (err) {
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-        return res.status(201).json({ id: newUser.insertedId, email });
-      });
-    });
+    const hashedPassword = sha1(password);
+
+    try {
+      const newUser = await dbClient.users.insertOne({ email, password: hashedPassword });
+      return res.status(201).json({ id: newUser.insertedId, email });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
   },
 };
 
